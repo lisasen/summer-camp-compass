517891b1489486274755f111535acfd305c9873e
lisasen <lisa.iyer@harvardbusiness.org>
2025-02-01 21:14:17 -0500

    gh-pages pre step

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Summer Camp Compass</h1>
+      <p>Find the best summer camps near you!</p>
+    </div>
+  );
+}
+
+export default App;
